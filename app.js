4c09dbee7f018c4cf519cff774b130bdd21ecd0f
JamesSenva <jamessenva@gmail.com>
2022-06-22 22:58:13 +0530

    Add basic pseudocode for the game

---
+++ app.js
@@ -0,0 +1,29 @@
+// rock, paper, or scissors
+
+// three choices for both you and computer
+
+// default scores for both YOU and COMPUTER is 0
+
+// if you choose ROCK and computer choose SCISSORS - YOU win
+// 1 point added to your score
+
+// if you choose PAPER and computer choose ROCK - YOU win
+// 1 point added to your score
+
+// if you choose SCISSORS and computer choose PAPER - YOU win
+// 1 point added to your score
+
+// if you choose SCISSORS and computer choose ROCK - COMPUTER wins
+// 1 point added to computer's score
+
+// if you choose ROCK and computer choose PAPER - COMPUTER wins
+// 1 point added to computer's score
+
+// if you choose PAPER and computer choose SCISSORS - COMPUTER wins
+// 1 point added to computer's score
+
+// if you and computer choose the same - TIE
+
+// play 3 times, whoever wins the most is the winner
+// after 3 attempts, whoever has more points wins
+
