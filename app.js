310c49ceaf6be8f31403c39ab173f1c8628971b1
JamesSenva <jamessenva@gmail.com>
2022-06-24 23:14:30 +0530

    Modify game which keeps score and declare winner

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 // rock, paper, or scissors
 
-// default scores for both YOU and COMPUTER is 0
-let playerScore;
-let computerScore;
-
 // three choices for both you and computer
 const roshambo = ['ROCK', 'PAPER', 'SCISSORS']
 
@@ -31,48 +27,60 @@ function computerPlay(){
 // if you choose SCISSORS and computer choose PAPER - YOU win
 // 1 point added to your score
 
- function playerPlay(){
-    let player = prompt(roshambo.join(' - ') + ' !');
-    return(player.toUpperCase());
+function playerPlay(){
+    let playerChoice = prompt(roshambo.join(' - ') + ' !');
+    let player = roshambo.indexOf(playerChoice.toUpperCase());
+    return(player);
 }
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection == 'ROCK' && computerSelection == 2){
+    if(playerSelection == 0 && computerSelection == 2){
         console.log('You win! Rock beats Scissors. Humanity has found it\s saviour!');
         playerScore++;
-    } else if(playerSelection == 'PAPER' && computerSelection == 0){
+    } else if(playerSelection == 1 && computerSelection == 0){
         console.log('You win! Paper beats Rock. Humanity has found it\s saviour!');
         playerScore++;
-    } else if(playerSelection == 'SCISSORS' && computerSelection == 1){
+    } else if(playerSelection == 2 && computerSelection == 1){
         console.log('You win! Scissors beats Paper. Humanity has found it\s saviour!');
         playerScore++;
-    } else if(playerSelection == 'SCISSORS' && computerSelection == 0){
+    } else if(playerSelection == 2 && computerSelection == 0){
         console.log('AI is winning. You lost! Rock beats Scissors.');
         computerScore++;
-    } else if(playerSelection == 'ROCK' && computerSelection == 1){
+    } else if(playerSelection == 0 && computerSelection == 1){
         console.log('AI is winning. You lost! Paper beats Rock.');
         computerScore++;
-    } else if(playerSelection == 'PAPER' && computerSelection == 2){
+    } else if(playerSelection == 1 && computerSelection == 2){
         console.log('AI is winning. You lost! Scissors beats Paper.');
         computerScore++;
     } else{
         console.log('It\'s a TIE.')
     }
 }
-
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection);
 // if you and computer choose the same - TIE
+
+
+// default scores for both YOU and COMPUTER is 0
+let playerScore = 0;
+let computerScore = 0;
 
 // play 5 times, whoever wins the most is the winner
 // after 5 attempts, whoever has more points wins
 
-/* 
-function game(){
-    for (let i = 0; i < 5, i++) {
 
+function game(){
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = playerPlay();
+        const computerSelection = computerPlay();
+        playRound( playerSelection, computerSelection );
+    }
+    if (playerScore > computerScore) {
+        console.log(`You won by ${playerScore} rounds. You are the Champion!`);
+    } else if (playerScore == computerScore) {
+        console.log(`Match is TIED.`);
+    } else {
+        console.log(`You lost by ${computerScore} rounds. You are a looser!`);
     }
 }
- */
+
+game();
 
